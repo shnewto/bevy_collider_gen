0ed27be0750eda8b4ab818a142e17231b336dd0f
shnewto <github@shnewto.dev>
2023-12-02 10:08:02 -0700

    make march_edges symbol public

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ pub use crate::collider::single_heightfield_collider_translated;
 pub use crate::collider::single_polyline_collider_raw;
 pub use crate::collider::single_polyline_collider_translated;
 pub use crate::edge::image_to_edges;
+pub use crate::edge::march_edges;
 pub use crate::edge::multi_image_edge_translated;
 pub use crate::edge::multi_image_edges_raw;
 pub use crate::edge::single_image_edge_raw;
