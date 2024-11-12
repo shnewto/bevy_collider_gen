3a51cc0133d13571e323232077fba111b94e9e3d
salam <salamatbekboev2008@gmail.com>
2024-11-12 22:37:01 +0900

    `ImagePlugin` type changed to `default_nearest`

--- examples/avian2d_colliders.rs
+++ examples/avian2d_colliders.rs
@@ -174,6 +174,7 @@ fn main() {
     App::new()
         .add_plugins(
             DefaultPlugins
+                .set(ImagePlugin::default_nearest())
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "colliders".to_string(),
