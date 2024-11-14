5161d53140f29ec1be9c67ce02a109211c8c54d7
salam <salamatbekboev2008@gmail.com>
2024-11-15 07:15:12 +0900

    add: doc for public functions

--- src/avian2d.rs
+++ src/avian2d.rs
@@ -32,6 +32,24 @@ fn to_collider(collider_type: ColliderType, points: Vec<Vec2>) -> Option<Collide
     }
 }
 
+/// Generates a collider from the provided image.
+///
+/// This function processes the input image and creates a collider of the specified type.
+/// The collider can be translated based on the `translate` flag.
+///
+/// # Parameters
+/// - `image`: The input image from which to generate the collider. The type `I` must implement `From<I>` for `Edges`.
+/// - `collider_type`: Specifies the type of collider to create (e.g., `Polyline`, `ConvexHull`, etc.).
+/// - `translate`: A boolean flag indicating whether to apply translation during collider generation.
+///
+/// # Returns
+/// Returns an `Option<Collider>`. If the collider is successfully generated, it returns `Some(Collider)`,
+/// otherwise, it returns `None` if the generation fails.
+///
+/// # Example
+/// ```
+/// let collider = generate_collider(image, ColliderType::Polyline, true);
+/// ```
 #[must_use]
 pub fn generate_collider<I>(
     image: I,
@@ -48,6 +66,24 @@ where
     )
 }
 
+/// Generates multiple colliders from the provided image.
+///
+/// This function processes the input image and creates a vector of colliders of the specified type.
+/// Each collider can be translated based on the `translate` flag.
+///
+/// # Parameters
+/// - `image`: The input image from which to generate the colliders. The type `I` must implement `From<I>` for `Edges`.
+/// - `collider_type`: Specifies the type of colliders to create (e.g., `Polyline`, `ConvexHull`, etc.).
+/// - `translate`: A boolean flag indicating whether to apply translation during collider generation.
+///
+/// # Returns
+/// Returns a `Vec<Option<Collider>>`, which is a vector of optional colliders. Each element may be `Some(Collider)`
+/// if the collider is successfully generated, or `None` if the generation fails for that particular collider.
+///
+/// # Example
+/// ```
+/// let colliders = generate_colliders(image, ColliderType::Polyline, true);
+/// ```
 #[must_use]
 pub fn generate_colliders<I>(
     image: I,
