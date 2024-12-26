d8120045481fbf783dc30ab95c27a19ac7e6a56f
salam <salamatbekboev2008@gmail.com>
2024-12-26 14:21:06 +0900

    fix: heights translation

--- src/utils.rs
+++ src/utils.rs
@@ -7,11 +7,13 @@ pub fn heights_and_scale(mut points: Vec<UVec2>, height: u32) -> (Vec<f32>, Vec2
         .chunk_by_mut(|p1, p2| p1 == p2)
         .for_each(|ch| ch.sort_by(|p1, p2| p2.y.cmp(&p1.y)));
     points.dedup_by(|p1, p2| p1.x == p2.x);
+
+    let dy = height as f32 / 2.;
     let heights: Vec<f32> = points
         .windows(2)
         .flat_map(|win| {
             let (p1, p2) = (win[0], win[1]);
-            let y = -(p2.y.abs_diff(height / 2) as f32);
+            let y = dy - p2.y as f32;
             (p1.x..p2.x).map(move |_| y)
         })
         .collect();
