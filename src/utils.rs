76166fc330fbae2948913bc995e78de4d6b95db8
salam <salamatbekboev2008@gmail.com>
2025-01-07 02:24:31 +0900

    optimize function behavior

--- src/utils.rs
+++ src/utils.rs
@@ -11,20 +11,23 @@ pub fn heights_and_scale(mut points: Vec<UVec2>, anchor: Anchor) -> (Vec<f32>, V
     points.sort_by_cached_key(|p| p.x);
     points.dedup_by(|p1, p2| p1.x == p2.x);
 
-    let dy = anchor.size().map_or_else(
-        || center_of(&points).map_or(0., |center| center.y),
-        |size| size.y as f32 / 2.,
-    );
+    if let Some(dy) = anchor
+        .size()
+        .map(|size| size.y as f32 / 2.)
+        .or_else(|| center_of(&points).map(|center| center.y))
+    {
+        let heights: Vec<f32> = points
+            .windows(2)
+            .flat_map(|win| {
+                let (p1, p2) = (win[0], win[1]);
+                let y = dy - p2.y as f32;
+                (p1.x..p2.x).map(move |_| y)
+            })
+            .collect();
 
-    let heights: Vec<f32> = points
-        .windows(2)
-        .flat_map(|win| {
-            let (p1, p2) = (win[0], win[1]);
-            let y = dy - p2.y as f32;
-            (p1.x..p2.x).map(move |_| y)
-        })
-        .collect();
-
-    let scale = Vec2::new(heights.len() as f32, 1.);
-    (heights, scale)
+        let scale = Vec2::new(heights.len() as f32, 1.);
+        (heights, scale)
+    } else {
+        (Vec::new(), Vec2::ONE)
+    }
 }
